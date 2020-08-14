60ffbe19a4ec6c8073faceb463605ec402f86d84
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-14 10:23:21 -0700

    submission before afternoon reattempt

--- data/migrations/20200814092134_howto_tables.js
+++ data/migrations/20200814092134_howto_tables.js
@@ -1,6 +1,11 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('Projects', tbl => {
+      tbl.increments();
+      tbl.string('Name', 255).notNullable();
+      tbl.string('Description');
+      tbl.boolean('Completed').defaultTo(false);
+  })
 };
 
 exports.down = function(knex) {
